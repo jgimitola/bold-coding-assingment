b6bdfb02adaee848877da78447ea45f08a4e5d9a
Jesus Imitola <47477274+jgimitola@users.noreply.github.com>
2024-06-22 11:31:50 -0500

    chore: add assets and configure head tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,7 +30,24 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="es">
-        <Head />
+        <Head>
+          <meta name="description" content="Bold dashboard page" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/assets/images/favicon-16x16.png"
+          />
+
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/assets/images/favicon-32x32.png"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
