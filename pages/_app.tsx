a6cf10e4dd4de96c2e69de70ad69a6f221485023
Jesus Imitola <47477274+jgimitola@users.noreply.github.com>
2024-06-22 11:44:02 -0500

    chore: install and configure dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,34 @@
 import type { AppProps } from 'next/app';
 
+import {
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import GlobalStyle from '@/shared/components/GlobalStyle';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      retryOnMount: false,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <HydrationBoundary state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </HydrationBoundary>
+        <ReactQueryDevtools initialIsOpen={false} position="right" />
+      </QueryClientProvider>
     </>
   );
 }
